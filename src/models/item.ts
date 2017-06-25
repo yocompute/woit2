51ae1ad6dd3b800e25888764fef3e7a0708e3990
zlkca <zlk1979@hotmail.com>
2017-06-25 11:05:47 -0400

    Fix upload not match new database schema issue

--- src/models/item.ts
+++ src/models/item.ts
@@ -4,37 +4,30 @@ export class Item{
 	constructor(
 		public title:string = '',
 		public description:string = '',
-		public code:string = '',
 		public dimension:string = '',
 		public author:string = '',
 		public year:string = '',
 		public type:string = '',		// photo, picutre
 		public source:string = '',		// original, market
 		public style:string = '',		// contemporary, morden
-		public price:number = null,
-		public currency:string = '',	// usd, cad, cny
 		public n_copies: number = null,
 		public fpath:string = null,
 		public created:string = '',
 		public updated:string = '',
-		public owner:User = null,
+		public user:User = null,
 		public id:string=''
 	){
 		this.title = title;
 		this.description = description;
-		this.code = code;
 		this.dimension = dimension;
 		this.author = author;
 		this.year = year;
 		this.type = type;
 		this.source = source;
 		this.style = style;
-		this.price = this.price;
-		this.currency = this.currency;
 		this.n_copies = n_copies;
 		this.fpath = fpath;
-		this.price = price;
-		this.owner = owner;
+		this.user = user;
 		this.id = id;
 	}
 }
